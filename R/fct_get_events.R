a6f10bce63787ea2e354a8a8fcd9456a337f2be0
Ozayr Mohammed <ozayr.mohammed@nhs.net>
2025-10-19 00:49:54 +0100

    Creates get_matches(). To preserve match data.

--- R/fct_get_events.R
+++ R/fct_get_events.R
@@ -2,18 +2,13 @@
 #'
 #' @description `get_events()` downloads events data using StatsBombr package.
 #'
-#' @param season_id Character. The season_id to return events for.
+#' @param matches A tibble. The output of `get_matches`.
 #' 
 #' @return List. A list of metadata and tibbles. One tibble per match, containing all event 
 #'   data for that match.
 #'
 #' @export 
-get_events <- function(season_id){
-  
-  comp <- StatsBombR::FreeCompetitions() |> 
-    dplyr::filter(.data$season_id == .env$season_id)
-  
-  matches <- StatsBombR::FreeMatches(comp)
+get_events <- function(matches){
   
   events <- matches |> 
     dplyr::group_nest(dplyr::row_number()) |> 
