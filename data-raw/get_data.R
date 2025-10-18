70145ab78bb42b318752872926a466f4c4ef48e4
Ozayr Mohammed <ozayr.mohammed@nhs.net>
2025-10-19 00:50:31 +0100

    get_data.R now uses get_matches. A vector of teams is also created and save as an .rds for ui inputs.

--- data-raw/get_data.R
+++ data-raw/get_data.R
@@ -1,8 +1,17 @@
 
 season_id <- 315 #Womens EURO 2025
 
-events <- get_events(season_id)
+matches <- get_matches(season_id)
+
+events <- get_events(matches)
 
 shots <- purrr::map(events, get_shots)
 
+teams <- matches |> 
+  dplyr::select(away_team.away_team_name, 
+                home_team.home_team_name) |> 
+  unlist() |> 
+  unique()
+
 saveRDS(shots, "inst/app/data/shots_womens_euro_2025.rds") 
+saveRDS(teams, "inst/app/data/teams_womens_euro_2025.rds")
