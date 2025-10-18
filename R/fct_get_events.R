ab54ea5ec0b8a1962b19e63d32242cf34957c614
Ozayr Mohammed <ozayr.mohammed@nhs.net>
2025-10-19 00:00:50 +0100

    updates get_events() to keep more metadata

--- R/fct_get_events.R
+++ R/fct_get_events.R
@@ -18,6 +18,14 @@ get_events <- function(season_id){
   events <- matches |> 
     dplyr::group_nest(dplyr::row_number()) |> 
     dplyr::pull(data) |> 
-    purrr::map(StatsBombR::get.matchFree)
+    purrr::map(\(x) list(match_id = x$match_id, 
+                         match_date = x$match_date, 
+                         match_week = x$match_week,
+                         season = x$season.season_name,
+                         home_team = x$home_team.country.name,
+                         away_team = x$away_team.country.name,
+                         competition_stage = x$competition_stage.name,
+                         shot_data = StatsBombR::get.matchFree)
+               )
   
 }
